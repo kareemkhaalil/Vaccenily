17f13e4e6dc1444645a97e070b6106940ad1efbc
karem <karem2003.kk@gmail.com>
2023-03-19 18:22:55 +0200

    kebo done

--- lib/bloc/articles/articlest_cubit.dart
+++ lib/bloc/articles/articlest_cubit.dart
@@ -1,179 +1,81 @@
 import 'package:bloc/bloc.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:dashborad/data/models/adminModel.dart';
 import 'package:dashborad/data/models/articlesModel.dart';
+import 'package:dashborad/data/remote/repo.dart';
 import 'package:file/file.dart';
-import 'package:firebase_storage/firebase_storage.dart';
-import 'package:image_picker_for_web/image_picker_for_web.dart';
-import 'package:meta/meta.dart';
+import 'package:flutter/material.dart';
 
 part 'articlest_state.dart';
 
-class ArticlestCubit extends Cubit<ArticlestState> {
-  ArticlestCubit() : super(ArticlestInitial());
-  AdminModel? adminModel;
-  ArticlesModel? articlesModel;
+class ArticlesCubit extends Cubit<ArticlesState> {
+  final Repository _repository;
 
-  void createArticle({
-    required String title,
-    required String body,
-    required String aid,
-    List<String>? image,
-    List<String>? tags,
-  }) {
-    try {
-      emit(ArticlestCreatingLoadingState());
-
-      FirebaseFirestore.instance
-          .collection('articles')
-          .doc(aid)
-          .set(articlesModel!.toJson());
-      emit(ArticlestCreateSuccessState());
-    } on FirebaseException catch (e) {
-      emit(ArticlestCreateErrorState(e.message!));
-      print(e.message);
-    }
-  }
-
-  //pick image
-  File? articlesImageFile;
-  var picker = ImagePickerPlugin();
-  Future pickImage() async {
-    emit(ArticlesPickImageLoadingState());
-    final pickedFile = await picker.pickFile();
-    if (pickedFile != null) {
-      emit(ArticlesPickImageSuccessState());
-      articlesImageFile!.path == pickedFile.path.toString();
-      print(pickedFile.path.toString());
-    } else {
-      print('No Image Selected');
-      emit(ArticlesPickImageErrorState('No Image Selected'));
-    }
-  }
-
-  void articlesUploadImages() {
-    emit(ArticlesUploadImageLoadingState());
-    FirebaseStorage.instance
-        .ref()
-        .child('articles')
-        .child(articlesImageFile!.path)
-        .child('image')
-        .putFile(articlesImageFile!)
-        .then((value) {
-      value.ref.getDownloadURL().then((value) {
-        updateArticle(image: [value]);
-        print(value);
-        emit(ArticlesUploadImageSuccessState());
-      });
-    }).catchError((error) {
-      emit(ArticlesUploadImageErrorState(error.toString()));
-    });
-  }
-
-  void updateArticle({
-    String? title,
-    String? body,
-    String? aid,
-    List<String>? image,
-    List<String>? tags,
-  }) {
-    try {
-      emit(ArticlesUpdateLoadingState());
-
-      FirebaseFirestore.instance
-          .collection('articles')
-          .doc(aid)
-          .update(articlesModel!.toJson());
-      emit(ArticlesUpdateSuccessState());
-    } on FirebaseException catch (e) {
-      emit(ArticlesUpdateErrorState(e.message!));
-      print(e.message);
-    }
-  }
-
-  void deleteArticle({
-    required String aid,
-  }) {
-    try {
-      emit(ArticlesDeleteLoadingState());
-      FirebaseFirestore.instance.collection('articles').doc(aid).delete();
-      emit(ArticlesDeleteSuccessState());
-    } on FirebaseException catch (e) {
-      emit(ArticlesDeleteErrorState(e.message!));
-      print(e.message);
-    }
-  }
-
+  ArticlesCubit(this._repository) : super(ArticlesInitial());
   Future<void> getAllArticles() async {
-    // try {
-    //   emit(ArticlesGetAllLoadingState());
-    //   FirebaseFirestore.instance.collection('articles').get().then((value) {
-    //     List<ArticlesModel> articles = [];
-    //     value.docs.forEach((element) {
-    //       articles.add(ArticlesModel.fromJson(element.data()));
-    //     });
-    //     emit(ArticlesLoaded(articles));
-    //   });
-    // } on FirebaseException catch (e) {
-    //   emit(ArticlesDeleteErrorState(e.message!));
-    //   print(e.message);
-    // }
-  }
+    emit(ArticlesLoading());
 
-  void getArticleById({
-    required String aid,
-  }) {
     try {
-      emit(ArticlesGetByIdLoadingState());
-      FirebaseFirestore.instance
-          .collection('articles')
-          .doc(aid)
-          .get()
-          .then((value) {
-        ArticlesModel article = ArticlesModel.fromJson(value.data()!);
-        emit(ArticlesGetByIdSuccessState([article]));
-      });
-    } on FirebaseException catch (e) {
-      emit(ArticlesGetByIdErrorState(e.message!));
-      print(e.message);
+      List<ArticlesModel> articles = await _repository.getAllArticles();
+      emit(ArticlesLoaded(articles));
+    } catch (e) {
+      emit(ArticlesError(e.toString()));
     }
   }
 
-  void getArticleByTag({
-    required String tag,
-  }) {
+  Future<void> getArticlesByAdminId() async {
+    emit(ArticlesLoading());
+
     try {
-      emit(ArticlesGetByTagLoadingState());
-      FirebaseFirestore.instance
-          .collection('articles')
-          .where('tags', arrayContains: tag)
-          .get()
-          .then((value) {
-        List<ArticlesModel> articles = [];
-        value.docs.forEach((element) {
-          articles.add(ArticlesModel.fromJson(element.data()));
-        });
-        emit(ArticlesGetByTagSuccessState(articles));
-      });
-    } on FirebaseException catch (e) {
-      emit(ArticlesGetByTagErrorState(e.message!));
-      print(e.message);
+      List<ArticlesModel> articles = await _repository.getArticlesByAdminId();
+      emit(ArticlesLoaded(articles));
+    } catch (e) {
+      emit(ArticlesError(e.toString()));
     }
   }
 
-  void addTagToArticle({
-    required String aid,
-    required String tag,
-  }) {
+  Future<void> addArticle(ArticlesModel article, [File? imageFile]) async {
+    emit(ArticlesLoading());
+
     try {
-      emit(ArticlesAddTagLoadingState());
-      FirebaseFirestore.instance.collection('articles').doc(aid).update({
-        'tags': FieldValue.arrayUnion([tag])
-      });
-      emit(ArticlesAddTagSuccessState());
-    } on FirebaseException catch (e) {
-      emit(ArticlesAddTagErrorState(e.message!));
-      print(e.message);
+      if (imageFile != null) {
+        String imageUrl = await _repository.uploadArticleImage(imageFile);
+        article = article.copyWith(image: [...(article.image ?? []), imageUrl]);
+      }
+      await _repository.addArticle(
+        article,
+        article.tags,
+      );
+      emit(ArticlesActionSuccess());
+    } catch (e) {
+      emit(ArticlesError(e.toString()));
+    }
+  }
+
+  Future<void> updateArticle(ArticlesModel article, [File? imageFile]) async {
+    emit(ArticlesLoading());
+
+    try {
+      if (imageFile != null) {
+        String imageUrl = await _repository.uploadArticleImage(imageFile);
+        article = article.copyWith(image: [...(article.image ?? []), imageUrl]);
+      }
+      await _repository.updateArticle(
+        article.id,
+        article,
+      );
+      emit(ArticlesActionSuccess());
+    } catch (e) {
+      emit(ArticlesError(e.toString()));
+    }
+  }
+
+  Future<void> deleteArticle(String articleId) async {
+    emit(ArticlesLoading());
+
+    try {
+      await _repository.deleteArticle(articleId);
+      emit(ArticlesActionSuccess());
+    } catch (e) {
+      emit(ArticlesError(e.toString()));
     }
   }
 }
--- lib/bloc/articles/articlest_state.dart
+++ lib/bloc/articles/articlest_state.dart
@@ -1,104 +1,33 @@
 part of 'articlest_cubit.dart';
 
-@immutable
-abstract class ArticlestState {}
+abstract class ArticlesState {}
 
-class ArticlestInitial extends ArticlestState {}
+class ArticlesInitial extends ArticlesState {}
 
-class ArticlestCreatingLoadingState extends ArticlestState {}
+class ArticlesLoading extends ArticlesState {}
 
-class ArticlestCreateSuccessState extends ArticlestState {}
-
-class ArticlestCreateErrorState extends ArticlestState {
-  final String error;
-  ArticlestCreateErrorState(this.error);
-}
-
-class ArticlesUpdateLoadingState extends ArticlestState {}
-
-class ArticlesUpdateSuccessState extends ArticlestState {}
-
-class ArticlesUpdateErrorState extends ArticlestState {
-  final String error;
-  ArticlesUpdateErrorState(this.error);
-}
-
-class ArticlesDeleteLoadingState extends ArticlestState {}
-
-class ArticlesDeleteSuccessState extends ArticlestState {}
-
-class ArticlesDeleteErrorState extends ArticlestState {
-  final String error;
-  ArticlesDeleteErrorState(this.error);
-}
-
-class ArticlesGetAllLoadingState extends ArticlestState {}
-
-class ArticlesGetAllSuccessState extends ArticlestState {
+class ArticlesSuccess extends ArticlesState {
   final List<ArticlesModel> articles;
-  ArticlesGetAllSuccessState(this.articles);
+
+  ArticlesSuccess(this.articles);
 }
 
-class ArticlesGetAllErrorState extends ArticlestState {
-  final String error;
-  ArticlesGetAllErrorState(this.error);
+class ArticlesFailure extends ArticlesState {
+  final String message;
+
+  ArticlesFailure(this.message);
 }
 
-class ArticlesGetByIdLoadingState extends ArticlestState {}
-
-class ArticlesGetByIdSuccessState extends ArticlestState {
-  final List<ArticlesModel> articles;
-  ArticlesGetByIdSuccessState(this.articles);
-}
-
-class ArticlesGetByIdErrorState extends ArticlestState {
-  final String error;
-  ArticlesGetByIdErrorState(this.error);
-}
-
-class ArticlesPickImageLoadingState extends ArticlestState {}
-
-class ArticlesPickImageSuccessState extends ArticlestState {}
-
-class ArticlesPickImageErrorState extends ArticlestState {
-  final String error;
-  ArticlesPickImageErrorState(this.error);
-}
-
-class ArticlesUploadImageLoadingState extends ArticlestState {}
-
-class ArticlesUploadImageSuccessState extends ArticlestState {}
-
-class ArticlesUploadImageErrorState extends ArticlestState {
-  final String error;
-  ArticlesUploadImageErrorState(this.error);
-}
-
-class ArticlesGetByTagLoadingState extends ArticlestState {}
-
-class ArticlesGetByTagSuccessState extends ArticlestState {
-  final List<ArticlesModel> articles;
-  ArticlesGetByTagSuccessState(this.articles);
-}
-
-class ArticlesGetByTagErrorState extends ArticlestState {
-  final String error;
-  ArticlesGetByTagErrorState(this.error);
-}
-
-class ArticlesAddTagLoadingState extends ArticlestState {}
-
-class ArticlesAddTagSuccessState extends ArticlestState {}
-
-class ArticlesAddTagErrorState extends ArticlestState {
-  final String error;
-  ArticlesAddTagErrorState(this.error);
-}
-
-class ArticlesLoaded extends ArticlestState {
+class ArticlesLoaded extends ArticlesState {
   final List<ArticlesModel> articles;
 
   ArticlesLoaded(this.articles);
-
-  List<ArticlesModel> get articlesData => articles;
 }
+
+class ArticlesError extends ArticlesState {
+  final String message;
+
+  ArticlesError(this.message);
+}
+
+class ArticlesActionSuccess extends ArticlesState {}
